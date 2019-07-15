24e615a7faeaebd823ae6bedce7dbedf16dc75f0
unknown <rossyfrancadev@gmail.com>
2019-07-15 19:06:45 -0300

    basic structure

---
+++ apiBuffalos/apiBuffalos/src/main/java/com/franca/apiBuffalos/ApiBuffalosApplication.java
@@ -0,0 +1,13 @@
+package com.franca.apiBuffalos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBuffalosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBuffalosApplication.class, args);
+	}
+
+}
